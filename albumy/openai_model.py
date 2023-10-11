261a0048becceae8ff5ae53285b2bdad77903551
Karen Liu <102920133+karenliu5@users.noreply.github.com>
2023-10-11 17:31:25 -0400

    added function for generating description

--- albumy/openai_model.py
+++ albumy/openai_model.py
@@ -6,11 +6,26 @@ import credentials
 # Have to set own API key in environment
 openai.api_key = credentials.OPENAI_API_KEY
 
-def get_completion(tags, model="gpt-3.5-turbo"):
+def get_alt_completion(tags, model="gpt-3.5-turbo"):
     print("tags:")
     print(tags)
     
     prompt = "Write an appropriate alternative text caption (limit to 25 words) for an image that is described using top keywords: {}".format(', '.join(tags))
+
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.2
+    )
+    
+    return response.choices[0].message["content"]
+
+def get_description_completion(tags, model="gpt-3.5-turbo"):
+    print("tags:")
+    print(tags)
+    
+    prompt = "Write an appropriate, fun instagram caption (limit to 10 words) for an image that is described using top keywords: {}".format(', '.join(tags))
 
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
